295d008a4d639b04c997cc575d4dd2e5d7585928
anykord <101556060+anykord@users.noreply.github.com>
2025-12-05 08:25:08 +0300

    Update adv-top.php

--- public/adv-top.php
+++ public/adv-top.php
@@ -3,8 +3,8 @@
 $ADV_ENABLED = true;  // false — реклама отключится полностью
 
 if ($ADV_ENABLED):
-    $adv_url = "https://your-adv-link.com/xyz";      // ← сюда ссылку
-    $adv_img = "https://your-host.com/adv/728x90.jpg"; // ← сюда баннер
+    $adv_url = "https://t.leads.tech/click/401/686/";      // ← сюда ссылку
+    $adv_img = "https://sun6-21.userapi.com/Kz1wtMj4XfR-I0yRlbNR7u8TNNMJSnidPmTcGw/deFGDWcQ8i8.jpg"; // ← сюда баннер
 ?>
 <div class="adv-top">
     <a href="<?=$adv_url?>" rel="nofollow" target="_blank">
